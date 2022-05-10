d89b54fe4358bbb91951ef279c5ea5141ecd7930
James Jacob <j.c>
2022-05-10 13:30:05 +0530

    Second commit

--- src/main/java/com/stacksimplify/restservices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
@@ -3,6 +3,7 @@ package com.stacksimplify.restservices.springbootbuildingblocks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Springboot
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
